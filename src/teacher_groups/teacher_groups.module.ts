a47d3da0309f4aaa9220a6391cfef25ad4887eab
baxti <ismonaliyevboqitali7@gmail.com>
2025-05-26 00:03:22 +0500

    Tabillarga graphql yozildi

--- src/teacher_groups/teacher_groups.module.ts
+++ src/teacher_groups/teacher_groups.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TeacherGroupsService } from './teacher_groups.service';
 import { TeacherGroupsController } from './teacher_groups.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { StudentGroup } from '../student_groups/entities/student_group.entity';
 import { TeacherGroupResolver } from './teacher_groups.resolver';
+import { TeacherGroup } from './entities/teacher_group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudentGroup])],
+  imports: [TypeOrmModule.forFeature([TeacherGroup])],
   controllers: [TeacherGroupsController],
   providers: [TeacherGroupsService, TeacherGroupResolver],
 })
